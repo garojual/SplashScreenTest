6b9705edc69098e3cfedf8d8ad87039bf0823d7c
Juan Alejandro Gallego Rodriguez <garojual@gmail.com>
2022-02-24 22:00:37 -0500

    Added all the CSS and styles

--- src/application/SplashScreen.java
+++ src/application/SplashScreen.java
@@ -3,16 +3,14 @@ package application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 
@@ -26,9 +24,10 @@ public class SplashScreen extends ScrollPane {
     private HBox auxHBOX;
 
     private Button leftButton;
-    private Button centerButton;
+    private ProgressBar splashProgressBar;
     private Button rightButton;
-    private Button auxButton;
+    private Button skipButton;
+
 
     public SplashScreen(){
         super();
@@ -38,11 +37,15 @@ public class SplashScreen extends ScrollPane {
 
     private void createInterface(){
 
+        // = new ImageView(getClass().getResource("/Resources/imgs/favpng_arrow-keys-clockwise-material-design.png").toExternalForm());
+
         leftVBox = new VBox();
         leftVBox.setAlignment(Pos.CENTER);
-        leftVBox.setStyle("-fx-background-color: #416b8d;");
         leftVBox.setPrefWidth(150);
-        leftButton = new Button("BackButton");
+        leftButton = new Button();
+        leftButton.setId("leftButton");
+        leftButton.setPrefWidth(50);
+        leftButton.setPrefHeight(50);
         leftVBox.getChildren().add(leftButton);
 
         centerVBox = new VBox();
@@ -50,30 +53,36 @@ public class SplashScreen extends ScrollPane {
         centerVBox.setAlignment(Pos.TOP_CENTER);
         //centerVBox.setStyle("-fx-background-color: #e77d3d;");
         centerVBox.setPrefWidth(500);
-        centerButton = new Button("Progress Bar");
-        centerVBox.getChildren().add(centerButton);
+        splashProgressBar = new ProgressBar(0.25);
+        splashProgressBar.setId("splashProgressBar");
+        centerVBox.getChildren().add(splashProgressBar);
 
 
         rightVBox = new VBox();
         rightVBox.setAlignment(Pos.TOP_CENTER);
-        rightVBox.setStyle("-fx-background-color: #8d4180;");
+        //rightVBox.setStyle("-fx-background-color: #8d4180;");
         rightVBox.setPrefWidth(150);
-        rightButton = new Button("Next Button");
+        rightButton = new Button();
+        rightButton.setId("rightButton");
+        rightButton.setPrefSize(50, 50);
         rightButton.setTranslateY(140);
 
         auxHBOX = new HBox();
         auxHBOX.setAlignment(Pos.TOP_CENTER);
-        auxHBOX.setStyle("-fx-background-color: #7bb06f;");
+        //auxHBOX.setStyle("-fx-background-color: #7bb06f;");
         auxHBOX.setPrefHeight(150);
-        auxButton = new Button("Skip");
-        auxHBOX.getChildren().add(auxButton);
+        skipButton = new Button("S K I P");
+        skipButton.setId("skipButton");
+        skipButton.setTranslateY(30);
+        auxHBOX.getChildren().add(skipButton);
 
         rightVBox.getChildren().add(auxHBOX);
         rightVBox.getChildren().add(rightButton);
 
         mainHBox = new HBox();
+        mainHBox.setId("mainHBox");
         mainHBox.setPrefSize(800,600);
-        mainHBox.setStyle("-fx-background-color: #7bb06f;");
+        //mainHBox.setStyle("-fx-background-color: #7bb06f;");
         mainHBox.getChildren().add(leftVBox);
         mainHBox.getChildren().add(centerVBox);
         mainHBox.getChildren().add(rightVBox);
