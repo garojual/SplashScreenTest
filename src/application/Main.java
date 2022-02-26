4cac417c372921603beb6fc3017693414ae9b95d
Juan Alejandro Gallego Rodriguez <garojual@gmail.com>
2022-02-26 18:11:22 -0500

    Added comments and separate some lines of code into blocks to improve readability

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,21 +3,53 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
+	/*
+	* sceneNumber controls for now which part of the GUI is displayed
+	* 0   = Splash Screen
+	* 1   = Register Screen
+	* 2++ = Reset Password Screen
+	* IMPORTANT = Make the classes for all the other GUI's
+	*/
+	int sceneNumber = 1;
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			SplashScreen root = new SplashScreen();
-			Scene scene = new Scene(root,800,600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Music Player - Splash");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		if (sceneNumber == 0) {
+			try {
+				SplashScreen root = new SplashScreen();
+				Scene scene = new Scene(root, 800, 600);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setTitle("Music Player - Splash");
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else if(sceneNumber == 1){
+			try {
+				SplashScreen root = new SplashScreen();
+				Scene scene = new Scene(root, 800, 600);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setTitle("Music Player - Register");
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else{
+			try {
+				SplashScreen root = new SplashScreen();
+				Scene scene = new Scene(root, 800, 600);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setTitle("Music Player - Reset Password");
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
