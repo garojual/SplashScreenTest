5818eec6e95d36c6665caa0b666641a99f2682e1
Juliana <julianareina.c@hotmail.com>
2022-02-28 23:02:20 -0500

    Added registerScreen.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,14 +7,14 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 public class Main extends Application {
-	/*HHHHHHHHh
+	/*
 	* sceneNumber controls for now which part of the GUI is displayed
 	* 0   = Splash Screen
 	* 1   = Register Screen
 	* 2++ = Reset Password Screen
 	* IMPORTANT = Make the classes for all the other GUI's
 	*/
-	int sceneNumber = 3;
+	int sceneNumber = 2;
 	Image icon = new Image(getClass().getResourceAsStream("/Resources/imgs/32x32logo.png"));
 
 	@Override
@@ -34,12 +34,13 @@ public class Main extends Application {
 			}
 		}
 		//Register Screen
-		else if(sceneNumber == 1){
+		else if(sceneNumber == 1) {
+			FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/Resources/fxml/registerScreen.fxml"));
+			Scene scene;
 			try {
-				SplashScreen root = new SplashScreen();
-				Scene scene = new Scene(root, 800, 600);
+				scene = new Scene(fxmlLoader.load(), 800, 600);
 				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-				primaryStage.setTitle("Music Player - Register");
+				primaryStage.setTitle("Music Player - Register Screen");
 				primaryStage.getIcons().add(icon);
 				primaryStage.setScene(scene);
 				primaryStage.show();
